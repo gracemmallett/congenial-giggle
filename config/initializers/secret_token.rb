c698fdc4f42da8cf4b479f0c05aad5795ea77959
Grace Mallett <gmmallet@bowdoin.edu>
2017-05-22 21:40:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gmmallett::Application.config.secret_key_base = 'd57e34b9d4ae6357a0dba6a774df77d86ceb05bbc2fce1a789192c12ba611cd56734c960f56ff293f7e7706a5fe8c14e1aaa465e1057a2b823e06965c183c6f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gmmallett::Application.config.session_store :cookie_store, key: '_gmmallett_session'
